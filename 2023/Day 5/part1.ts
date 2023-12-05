4943e72c2c6ee35b870f2dcfba5d4b1a64caf3f8
stevenchencb <stevenchen@hotmail.de>
2023-12-05 19:49:48 +0100

    Day 5 (messy)

---
+++ 2023/Day 5/part1.ts
@@ -0,0 +1,53 @@
+import { extractWithRegex, getFileInput, getFileLines } from '../../utils';
+
+const input = await getFileInput('./input.txt');
+const seedsAndMapsStrings = input.split('\n\n');
+
+const seedToSoilMap = getMap(seedsAndMapsStrings, 'seed-to-soil');
+const soilToFertMap = getMap(seedsAndMapsStrings, 'soil-to-fertilizer');
+const fertToWaterMap = getMap(seedsAndMapsStrings, 'fertilizer-to-water');
+const waterToLightMap = getMap(seedsAndMapsStrings, 'water-to-light');
+const lightToTempMap = getMap(seedsAndMapsStrings, 'light-to-temperature');
+const tempToHumidMap = getMap(seedsAndMapsStrings, 'temperature-to-humidity');
+const humidToLocationMap = getMap(seedsAndMapsStrings, 'humidity-to-location');
+
+const seeds = [...seedsAndMapsStrings[0].matchAll(/\d+/g)].map((m) => Number.parseInt(m[0]));
+
+const locations = seeds.map((n) =>
+	map(
+		map(map(map(map(map(map(n, seedToSoilMap), soilToFertMap), fertToWaterMap), waterToLightMap), lightToTempMap), tempToHumidMap),
+		humidToLocationMap
+	)
+);
+
+console.log(locations.sort((a, b) => a - b));
+
+function getMap(seedsAndMapsStrings: string[], mapName: string) {
+	const mapLines =
+		seedsAndMapsStrings
+			.find((s) => s.includes(mapName))
+			?.split('\n')
+			?.slice(1) ?? [];
+	const resultMap: number[][] = [];
+
+	for (const line of mapLines) {
+		const regex = /(?<destStart>\d+) (?<srcStart>\d+) (?<range>\d+)/;
+		const destStart = Number.parseInt(extractWithRegex(line, regex, 'destStart'));
+		const srcStart = Number.parseInt(extractWithRegex(line, regex, 'srcStart'));
+		const range = Number.parseInt(extractWithRegex(line, regex, 'range'));
+
+		resultMap.push([srcStart, destStart, range]);
+	}
+
+	return resultMap;
+}
+
+function map(n: number, map: number[][]): number {
+	for (const srcDestRange of map) {
+		if (srcDestRange[0] <= n && srcDestRange[0] + srcDestRange[2] >= n) {
+			return srcDestRange[1] + (n - srcDestRange[0]);
+		}
+	}
+
+	return n;
+}
---
+++ 2023/Day 5/part2.ts
@@ -0,0 +1,77 @@
+import { extractWithRegex, getFileInput } from '../../utils';
+
+const input = await getFileInput('./input.txt');
+const seedsAndMapsStrings = input.split('\n\n');
+
+const seedToSoilMap = getMap(seedsAndMapsStrings, 'seed-to-soil');
+const soilToFertMap = getMap(seedsAndMapsStrings, 'soil-to-fertilizer');
+const fertToWaterMap = getMap(seedsAndMapsStrings, 'fertilizer-to-water');
+const waterToLightMap = getMap(seedsAndMapsStrings, 'water-to-light');
+const lightToTempMap = getMap(seedsAndMapsStrings, 'light-to-temperature');
+const tempToHumidMap = getMap(seedsAndMapsStrings, 'temperature-to-humidity');
+const humidToLocationMap = getMap(seedsAndMapsStrings, 'humidity-to-location');
+
+const seeds = [...seedsAndMapsStrings[0].matchAll(/\d+/g)].map((m) => Number.parseInt(m[0]));
+
+const seedRanges = seeds.reduce<number[][]>((acc, curr, i) => {
+	if (i % 2 === 0) {
+		acc[Math.floor(i / 2)] = [curr];
+	} else {
+		acc[Math.floor(i / 2)][1] = curr;
+	}
+	return acc;
+}, []);
+
+console.log(findSmallestLocationNumber());
+
+function findSmallestLocationNumber(): number {
+	for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+		const seed = reverseMap(
+			reverseMap(
+				reverseMap(
+					reverseMap(reverseMap(reverseMap(reverseMap(i, humidToLocationMap), tempToHumidMap), lightToTempMap), waterToLightMap),
+					fertToWaterMap
+				),
+				soilToFertMap
+			),
+			seedToSoilMap
+		);
+
+		for (const range of seedRanges) {
+			if (seed >= range[0] && seed < range[0] + range[1]) {
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+function getMap(seedsAndMapsStrings: string[], mapName: string) {
+	const mapLines =
+		seedsAndMapsStrings
+			.find((s) => s.includes(mapName))
+			?.split('\n')
+			?.slice(1) ?? [];
+	const resultMap: number[][] = [];
+
+	for (const line of mapLines) {
+		const regex = /(?<destStart>\d+) (?<srcStart>\d+) (?<range>\d+)/;
+		const destStart = Number.parseInt(extractWithRegex(line, regex, 'destStart'));
+		const srcStart = Number.parseInt(extractWithRegex(line, regex, 'srcStart'));
+		const range = Number.parseInt(extractWithRegex(line, regex, 'range'));
+
+		resultMap.push([srcStart, destStart, range]);
+	}
+
+	return resultMap;
+}
+
+function reverseMap(n: number, map: number[][]): number {
+	for (const srcDestRange of map) {
+		if (srcDestRange[1] <= n && srcDestRange[1] + srcDestRange[2] - 1 >= n) {
+			return n - srcDestRange[1] + srcDestRange[0];
+		}
+	}
+
+	return n;
+}
